e42b5c041685476e3936817e70382bfb8efbdfe3
Kajak <wyslijtokajaki@gmail.com>
2022-11-30 00:33:05 -0800

    assaas

--- thirdApk/app/src/main/java/com/example/thirdapk/MainActivity.java
+++ thirdApk/app/src/main/java/com/example/thirdapk/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.thirdapk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.aktywnosci:
+                return true;
+            case R.id.profil:
+                return true;
+            case R.id.wyloguj:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 }
